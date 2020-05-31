fdf799cb6d323dd346fc9f4c5b266d4aa9d8fd68
Adrian Lee <adrianlee0118@gmail.com>
2020-05-30 21:39:26 -0700

    formatted index.html and pulled in redux, react-redux

--- src/js/components/Form.js
+++ src/js/components/Form.js
@@ -1,38 +1,38 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
 
 class Form extends Component {
-    constructor(){
-        super();
-        this.state = {
-            value: ''
-        };
-        this.handleChange = this.handleChange.bind(this);
-    }
+  constructor() {
+    super();
+    this.state = {
+      value: "La la",
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-    handleChange(event){
-        const { value } = event.target;
-        this.setState(() => {
-            return {
-                value
-            };
-        });
-    }
+  handleChange(event) {
+    const { value } = event.target;
+    this.setState(() => {
+      return {
+        value,
+      };
+    });
+  }
 
-    render() {
-        return (
-            <form>
-                <input 
-                    type = 'text'
-                    value = {this.state.value}
-                    onChange = {this.handleChange}
-                />
-            </form>
-        );
-    }
+  render() {
+    return (
+      <form>
+        <input
+          type="text"
+          value={this.state.value}
+          onChange={this.handleChange}
+        />
+      </form>
+    );
+  }
 }
 
 export default Form;
 
-const wrapper = document.getElementById('container');
+const wrapper = document.getElementById("container");
 warpper ? ReactDOM.render(<Form />, wrapper) : false;
