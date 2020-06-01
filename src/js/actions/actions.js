a022bf62bb97fbfeea80ff2304823020bd3bf433
Adrian Lee <adrianlee0118@gmail.com>
2020-05-31 23:26:15 -0700

    make action types into const strings

--- src/js/actions/actions.js
+++ src/js/actions/actions.js
@@ -1,20 +1,24 @@
+export const ADD_ITEM = 'ADD_ITEM';
+export const DELETE_ITEM = 'DELETE_ITEM';
+export const EDIT_ITEM = 'EDIT_ITEM';
+
 const addItem = (name, quantity, price, total) => {
     return {
-        type: 'ADD_ITEM',
+        type: ADD_ITEM,
         payload: {name, quantity, price, total}
     };
 };
 
 const deleteItem = (index) => {
     return {
-        type: 'DELETE_ITEM',
+        type: DELETE_ITEM,
         payload: {index}
     };
 };
 
 const editItem = (name, quantity, price) => {
     return {
-        type: 'EDIT_ITEM',
+        type: EDIT_ITEM,
         payload: {name, quantity, price}
     };
 };
