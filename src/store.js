6609716ea70a6802c0d9f675e9d12caeb4744452
Adrian Lee <adrianlee0118@gmail.com>
2020-06-01 19:25:44 -0700

    removed css-loader and style-loader dependencies

--- src/store.js
+++ src/store.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import { Provider } from 'react-redux';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import React from "react";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import getMuiTheme from "material-ui/styles/getMuiTheme";
+import darkBaseTheme from "material-ui/styles/baseThemes/darkBaseTheme";
 
-import rootReducer from './reducer';
-import App from './components/app';
+import rootReducer from "./reducer";
+import App from "./components/app";
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
-export default createStore(reducer);
+export default () => (
+  <Provider store={store}>
+    <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+      <App />
+    </MuiThemeProvider>
+  </Provider>
+);
