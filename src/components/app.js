6a2e13857e43600a4dc3260ac942da908e8daeb6
Adrian Lee <adrianlee0118@gmail.com>
2020-06-01 17:53:59 -0700

    added styles to app.js

--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,17 @@
-import React from 'react';
-import NavBar from './navBar';
-import Invoice from './invoice';
+import React from "react";
+import NavBar from "./navBar";
+import Invoice from "./invoice";
 
+const styles = {
+  mainContainer: {
+    display: "flex-inline",
+    flexDirection: "row",
+    backgroundColor: "black",
+    height: "98vh",
+  },
+  invoiceContainer: {
+    display: "flex",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+};
