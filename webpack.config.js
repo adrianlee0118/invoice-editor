ec59449b3eb0dbece7a5b7afedbb7a2105cc4959
Adrian Lee <adrianlee0118@gmail.com>
2020-06-01 19:51:23 -0700

    added hot module replacement, removed HTML and CSS loading

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,32 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const webpack = require('webpack');
 
 module.exports = {
+  entry: [
+    'react-hot-loader/patch',
+    './src/index.js',
+  ],
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
+        use: ['babel-loader'],
       },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader",
-          },
-        ],
-      },
-      {
-        test: /\.css$/,
-        use: [
-            'style-loader', 'css-loader'
-        ],
-      }
     ],
   },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
   plugins: [
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html",
-    })
+    new webpack.HotModuleReplacementPlugin(),
   ],
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+  },
 };
