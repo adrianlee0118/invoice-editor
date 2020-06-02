3f5a11948aa505f09012314af639b7dadfebd0d5
Adrian Lee <adrianlee0118@gmail.com>
2020-06-01 17:41:11 -0700

    added navigation bar

---
+++ src/js/components/navBar.js
@@ -0,0 +1,15 @@
+  
+import React from 'react';
+import AppBar from 'material-ui/AppBar';
+
+/*
+  Navigation bar function component for additional styling purposes.
+*/
+const NavBar = () => (
+  <AppBar
+    title="Invoice Editor"
+    showMenuIconButton={false}
+  />
+);
+
+export default NavBar;
