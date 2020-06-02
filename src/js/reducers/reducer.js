332fc1f03d244395275b69d41e0e4c53e022e03f
Adrian Lee <adrianlee0118@gmail.com>
2020-06-01 17:28:29 -0700

    changed functions in reducer to reflect actions nomenclature and added operations

--- src/js/reducers/reducer.js
+++ src/js/reducers/reducer.js
@@ -7,45 +7,36 @@ const initalState = {
     total: null,
 };
 
-const reducer = (state = initialState, action) => {
+const invoiceReducer = (state = initalState, action) => {
     switch (action.type) {
-        case ADD_ITEM:
-            updatedItems = [...state.items.items, action.payload];
-            subtotal = updatedItems.reduce((sum, item) => {
-                return sum + item.total
-            }, 0);
-            return {
-                items: {
-                    items: updatedItems
-                },
-                totals: {
-                    subtotal: subtotal,
-                    tax: subtotal * 0.05,
-                    total: subtotal * 1.05
-                }
-            };
-        case EDIT_ITEM:
-            return state;
-        case DELETE_ITEM:
-            updatedItems = state.items.items.filter((item, index) =>    //should be (item => item.index != action.payload.index)
-                index != action.payload.index
-            );
-            subtotal = newItems.reduce((sum,item) => {
-                return sum + item.total
-            }, 0);
-            return {
-                items: {
-                    items: updatedItems
-                },
-                totals: {
-                    subtotal: subtotal,
-                    tax: subtotal * 0.05,
-                    total: subtotal * 1.05
-                }
-            };
-        default:
-            return state;
+      case ADD_NEW_ITEM:
+        return {
+          ...state,
+          listItems: [...state.listItems, action.itemData],
+        };
+  
+      case REMOVE_ITEM:
+        return {
+          ...state,
+          listItems: state.listItems.filter((item, index) => index !== action.position),
+        };
+  
+      case CALCULATE_TAX_TOTAL:
+        const subTotal = state.listItems.reduce((acc, item) => acc + item.total, 0);
+        const tax = Number(((subTotal / 100) * 5).toFixed(2));
+        const total = Number((subTotal + tax).toFixed(2));
+        return {
+          ...state,
+          subTotal,
+          tax,
+          total,
+        };
+  
+      default:
+        return state;
     }
-};
-
-export default reducer;
+  };
+  
+  const reducer = combineReducers({ invoiceReducer });
+  
+  export default reducer;
