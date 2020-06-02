3168acac2e91a201e32db3436aada6259c99a0a5
Adrian Lee <adrianlee0118@gmail.com>
2020-06-01 18:03:49 -0700

    added middleware to store

--- src/store.js
+++ src/store.js
@@ -1,4 +1,14 @@
-import { createStore } from 'redux';
-import reducer from './js/reducers/reducer';
+import React from 'react';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+
+import rootReducer from './reducer';
+import App from './components/app';
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default createStore(reducer);
