16d5cc43038436ce25a272b9f878a85c3b6582cd
Adrian Lee <adrianlee0118@gmail.com>
2020-06-01 17:25:09 -0700

    added actions, changed arguments

--- src/js/actions/actions.js
+++ src/js/actions/actions.js
@@ -1,29 +1,54 @@
 export const ADD_NEW_ITEM = 'ADD_NEW_ITEM';
-export const EDIT_ITEM = 'EDIT_ITEM';
+export const CALCULATE_TAX_TOTAL = 'CALCULATE_TAX_TOTAL';
+export const REMOVE_ITEM = 'REMOVE_ITEM';
 
-const addItem = (itemData) => {
-    return {
-        type: ADD_NEW_ITEM,
-        itemData,
-    };
+
+const addNewItem = (itemData) => {
+  return {
+    type: ADD_NEW_ITEM,
+    itemData,
+  };
 };
 
 const removeItem = (position) => {
-    return {
-        type: DELETE_ITEM,
-        payload: {index}
-    };
+  return {
+    type: REMOVE_ITEM,
+    position,
+  };
 };
 
-const editItem = (name, quantity, price) => {
-    return {
-        type: EDIT_ITEM,
-        payload: {name, quantity, price}
-    };
+const calculateTotal = () => {
+  return {
+    type: CALCULATE_TAX_TOTAL,
+  };
 };
+
+const submitItem = ({ item, qty, price }) => {
+  const itemData = {
+    item,
+    qty,
+    price,
+    total: price * qty,
+  };
+  return (dispatch) => {
+    dispatch(addNewItem(itemData));
+    dispatch(calculateTotal());
+  };
+};
+
+// eslint-disable-next-line
+const deleteItem = (position) => {
+  return (dispatch) => {
+    dispatch(removeItem(position));
+    dispatch(calculateTotal());
+  };
+};
+
 
 export {
-    addItem,
-    deleteItem,
-    editItem
+  submitItem,
+  calculateTotal,
+  deleteItem,
+  addNewItem,
+  removeItem,
 };
