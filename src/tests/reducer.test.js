0e3c2633532bddc5df191267e0f4957d07ac6b12
Adrian Lee <adrianlee0118@gmail.com>
2020-06-03 13:56:46 -0700

    added unit testing for removing, editing items and calculating totals

--- src/tests/reducer.test.js
+++ src/tests/reducer.test.js
@@ -1,27 +1,100 @@
 import invoiceReducer from './reducers/reducer';
 import { ADD_NEW_ITEM, REMOVE_ITEM, CHANGE_ITEM, CALCULATE_TAX_TOTAL } from './actions/actions';
 
-describe('todos reducer', () => {
+describe('Invoice reducer testing', () => {
+
     it('should handle initial state', () => {
         expect(
-            rootReducer(undefined, {})
+            invoiceReducer(undefined, {})
         ).toEqual({
-            items: [],
+            listItems: [],
             subTotal: 0,
-            taxTotal: 0,
-            grandTotal: 0
+            tax: 0,
+            total: 0,
         });
     });
 
     it('should handle ADD_NEW_ITEM', () => {
 
-        let addItem = rootReducer({
+        let addItem = invoiceReducer({
             items: [],
             subTotal: 0,
             taxTotal: 0,
             grandTotal: 0
         }, {
             type: ADD_NEW_ITEM,
+            payload: {
+                name: 'Filing Cabinet',
+                quantity: 3,
+                price: 39.99
+            }
+        });
+
+        expect(addItem.items.length).toEqual(1);
+        expect(addItem.subTotal).toEqual(119.97);
+        expect(addItem.taxTotal).toEqual(5.9985);
+        expect(addItem.grandTotal).toEqual(125.9685);
+
+    });
+
+
+    it('should handle REMOVE_ITEM', () => {
+
+        let removeItem = invoiceReducer({
+            items: [],
+            subTotal: 0,
+            taxTotal: 0,
+            grandTotal: 0
+        }, {
+            type: REMOVE_ITEM,
+            payload: {
+                name: 'Filing Cabinet',
+                quantity: 3,
+                price: 39.99
+            }
+        });
+
+        expect(addItem.items.length).toEqual(1);
+        expect(addItem.subTotal).toEqual(119.97);
+        expect(addItem.taxTotal).toEqual(5.9985);
+        expect(addItem.grandTotal).toEqual(125.9685);
+
+    });
+
+
+    it('should handle CHANGE_ITEM', () => {
+
+        let changeItem = invoiceReducer({
+            items: [],
+            subTotal: 0,
+            taxTotal: 0,
+            grandTotal: 0
+        }, {
+            type: CHANGE_ITEM,
+            payload: {
+                name: 'Filing Cabinet',
+                quantity: 3,
+                price: 39.99
+            }
+        });
+
+        expect(addItem.items.length).toEqual(1);
+        expect(addItem.subTotal).toEqual(119.97);
+        expect(addItem.taxTotal).toEqual(5.9985);
+        expect(addItem.grandTotal).toEqual(125.9685);
+
+    });
+
+
+    it('should handle CALCULATE_TAX_TOTAL', () => {
+
+        let calculateItem = invoiceReducer({
+            items: [],
+            subTotal: 0,
+            taxTotal: 0,
+            grandTotal: 0
+        }, {
+            type: CALCULATE_TAX_TOTAL,
             payload: {
                 name: 'Filing Cabinet',
                 quantity: 3,
